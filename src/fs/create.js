421dcce3410fc9047fafd6d31627cb0a3436812f
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-27 15:00:53 +0300

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile } from "node:fs";
+import { access } from "node:fs/promises";
 const create = async () => {
-    // Write your code here 
+  // Write your code herenode -v
+  let fileContent = "I am fresh and young";
+  var filepath = "fs/files/fresh.txt";
+
+  try {
+    await access(filepath);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      writeFile(filepath, fileContent, (writeErr) => {
+        if (writeErr) throw writeErr;
+        console.log("The file was successfully saved!");
+      });
+    } else {
+      console.error("Error:", err.message);
+    }
+  }
 };
 
 await create();
