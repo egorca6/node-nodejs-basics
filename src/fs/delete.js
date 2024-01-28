3e9637d8e847bd27e066ded9c75a6c816b89ba59
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-28 23:31:00 +0300

    feat: add  delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { unlink } from "node:fs";
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  unlink("fs/files/fileToRemove.txt", (err) => {
+    if (err) {
+      console.error("FS operation failed");
+    } else {
+      console.log("fileToRemove.txt was deleted");
+    }
+  });
 };
 
 await remove();
