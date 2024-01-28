918d236e9245b7c44f376dcf6ec216d93e1e3be3
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-28 23:09:23 +0300

    add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { rename as renameFs, stat } from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+  stat("fs/files/properFilename.md", (err, stats) => {
+    if (stats) {
+      console.error("FS operation failed. File already exists");
+    } else {
+      renameFs(
+        "fs/files/wrongFilename.txt",
+        "fs/files/properFilename.md",
+        (err) => {
+          if (err) {
+            console.error(
+              "FS operation failed. wrongFilename.txt  doesn't exist"
+            );
+          }
+        }
+      );
+    }
+  });
 };
 
 await rename();
