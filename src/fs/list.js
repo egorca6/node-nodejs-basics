870b325eab77b6d4715cc98caad408cd0cd08b32
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-28 23:42:27 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import { readdir } from "node:fs/promises";
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const files = await readdir("fs/files");
+    for (const file of files) console.log(file);
+  } catch (err) {
+    console.error("FS operation failed");
+  }
 };
 
 await list();
