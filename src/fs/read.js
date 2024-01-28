ede7c0e06810cf04da01ba03b1385f422d61c4a8
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-28 23:48:58 +0300

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  async function logFile() {
+    try {
+      const filePath = resolve("fs/files/fileToRead.txt");
+      const contents = await readFile(filePath, { encoding: "utf8" });
+      console.log(contents);
+    } catch (err) {
+      console.error("FS operation failed");
+    }
+  }
+  logFile();
 };
 
 await read();
