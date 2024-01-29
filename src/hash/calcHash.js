b29b0571bd5ffa906cc06c60e070d1b59b73b243
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-29 13:21:08 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+const { createHash } = await import("node:crypto");
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const input = createReadStream("src/hash/files/fileToCalculateHashFor.txt");
+  input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
