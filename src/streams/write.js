99a42aae2c5a36c3239090e7c542aae8044b8de1
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-29 13:35:44 +0300

    feat: add streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,9 @@
+import { createWriteStream } from "node:fs";
+import { stdin } from "node:process";
 const write = async () => {
-    // Write your code here 
+  const output = createWriteStream("src/streams/files/fileToWrite.txt");
+  stdin.pipe(output);
+  console.log("Enter some text");
 };
 
 await write();
