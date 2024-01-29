2fa065b9d637463965c8768c69465f580f09b532
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-29 14:30:45 +0300

    feat: add zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const gzip = createGzip();
+  const source = createReadStream("src/zip/files/fileToCompress.txt");
+  const destination = createWriteStream("src/zip/files/archive.gz");
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    } else {
+      console.log("Complete!");
+    }
+  });
 };
 
 await compress();
