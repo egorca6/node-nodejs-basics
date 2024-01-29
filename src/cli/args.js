9d57529c367bd2ebbc00ff82bd4e6b713c980e3b
Yahor Tsytsarkin <egormail88@gmail.com>
2024-01-29 03:57:44 +0300

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
+import { argv } from "node:process";
 const parseArgs = () => {
-  // Write your code here
+  const args = argv.slice(2);
+
+  for (let i = 0; i < args.length; i = i + 2) {
+    let propName = args[i].replace(/^--/, "");
+    const value = args[i + 1];
+    console.log(`${propName} is ${value}`);
+  }
 };
 
 parseArgs();
